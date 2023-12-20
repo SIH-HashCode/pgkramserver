7cdf08d4ba42e1df32099af36e13b0f07e0e9efe
UTSAV MANDHANI <85448958+Utsav2929@users.noreply.github.com>
2023-12-20 05:42:58 +0530

    dashboard

---
+++ contontrollers/user.js
@@ -0,0 +1,24 @@
+import Seeker from '../models/seeker.js'; 
+
+
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+  console.log(req.body)
+    try {
+      // Find user by email and password
+      const user = await Seeker.findOne({ email, password });
+        console.log(user)
+      if (!user) {
+        // User not found
+        return res.status(401).json({ message: 'Invalid credentials' });
+      }
+  
+      
+  
+      res.status(200).json({ message: 'Login successful'});
+    } catch (error) {
+      console.error('Error during login:', error.message);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  };
